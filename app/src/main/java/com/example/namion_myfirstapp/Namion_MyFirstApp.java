b309c67750d0817705e01dd6477a2bc8dbd2eb3e
peachiess <marienelnamion@gmail.com>
2022-01-16 22:24:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/namion_myfirstapp/Namion_MyFirstApp.java
@@ -0,0 +1,14 @@
+package com.example.namion_myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Namion_MyFirstApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
